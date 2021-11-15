e57e26649905b56277aa9f742cc2d90bc093561b
Apleon <shara49750@gmail.com>
2021-11-15 17:27:46 +0300

    update

--- L6/main.cpp
+++ L6/main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include "lab6_1.hpp"
+#include <cmath>
 
 int main(){
     setlocale(LC_ALL, "ru");
     
     int side_a;
-    
     std::cin >> side_a;
-    
     std::cout << square_cout(side_a) << std::endl;
     
 }
