6d6f75011e9c401a21298c4ccd7a13b590a08fda
Apleon <shara49750@gmail.com>
2021-11-16 14:03:27 +0300

    update

--- L6/lab6_1.hpp
+++ L6/lab6_1.hpp
@@ -36,10 +36,10 @@ int length(double diameter, double p = 3.14){
     return diameter * p;
 }
 //
-//task 5.1
-//int v_of_cube(int a) {
-//    return pow(a, 3);
-//}
+task 5.1
+int v_of_cube(int a) {
+    return pow(a, 3);
+}
 
 ////task 5.2
 //int square_of_cube(int a) {
